4932d3f96fffa95f53637674d2c8530c5e3898be
Eric Austin <eaustin@gnresound.com>
2018-12-28 10:22:00 -0600

    Update RepoZip.ps1

--- RepoZip.ps1
+++ RepoZip.ps1
@@ -8,10 +8,34 @@ $url="https://github.com/austineric/$repo/archive/master.zip"
 $destination="C:$env:HOMEPATH\Documents"
 
 If (Test-Path $destination\$repo) {
-    Remove-Item $destination\$repo -Recurse -Force
+    Clear-Host
+    $response=Read-Host "The destination $destination\$repo already exists. Delete and replace? (y/n)"
+    If ($response -eq "y") {
+        Remove-Item $destination\$repo -Recurse -Force
+    }
+    Elseif ($response -eq "n") {
+        Write-Host "Cancelled. Exiting now." 
+        Pause
+        Exit
+    }
+    Else {Write-Host "Invalid response. Exiting now."
+        Pause
+        Exit
+    }
 }
 
-Invoke-WebRequest -Uri $url -OutFile "$destination\$repo.zip"
-Expand-Archive -Path "$destination\$repo.zip" -DestinationPath $destination
-Remove-Item "$destination\$repo.zip"
-Rename-Item -Path "$destination\$repo-master" -NewName $repo
+Try {
+    Invoke-WebRequest -Uri $url -OutFile "$destination\$repo.zip"
+    Expand-Archive -Path "$destination\$repo.zip" -DestinationPath $destination
+    Remove-Item "$destination\$repo.zip"
+    Rename-Item -Path "$destination\$repo-master" -NewName $repo
+    Write-Host "Completed."
+    Pause
+    Exit
+}
+Catch {
+    Write-Host "Download failed. Ensure the repo name is entered correctly and try again."
+    Pause
+    Exit
+}
+
